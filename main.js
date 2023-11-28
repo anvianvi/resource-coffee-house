5683c015a4356f91f5c4e69d64bd698a663f89aa
Pavel <verbipo@gmail.com>
2023-11-28 22:22:03 +0100

    feat: enjoy slider

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './style.scss'
 import './syles/header.scss'
 import './syles/hero.scss'
+import { generateEnjoyList, nextSlide, prevSlide } from './scripts/enjoy';
+import './syles/enjoy.scss'
 
-// import javascriptLogo from '/javascript.svg'
-// import viteLogo from '/vite.svg'
-
-
+generateEnjoyList()
