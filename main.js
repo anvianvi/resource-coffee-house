7955439f16f02b8303167102fee9fbdef9eb436b
Pavel <verbipo@gmail.com>
2023-11-29 11:38:49 +0100

    feat: about block

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import './style.scss'
 import './syles/header.scss'
 import './syles/hero.scss'
-import { generateEnjoyList, nextSlide, prevSlide } from './scripts/enjoy';
+import { generateEnjoyList } from './scripts/enjoy';
 import './syles/enjoy.scss'
+import './syles/about.scss'
+
 
 generateEnjoyList()
