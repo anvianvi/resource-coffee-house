08e20ba677c553778b8a2eaf8fbe3bcb18453ffc
Pavel <verbipo@gmail.com>
2023-11-29 13:58:11 +0100

    feat: mobile-apps section

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import './syles/hero.scss'
 import { generateEnjoyList } from './scripts/enjoy';
 import './syles/enjoy.scss'
 import './syles/about.scss'
+import './syles/mobile-app.scss'
+
 
 
 generateEnjoyList()
