367cb7c6051ec5ff25e4af0bf3f33742ec97b8d7
Pavel <verbipo@gmail.com>
2023-12-08 19:11:01 +0100

    feat: main page 1440-768-380

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import '/styles/global.scss'
 import '/styles/header.scss'
+import './scripts/burger-menu';
 import '/styles/hero.scss'
-import { generateEnjoyList } from './scripts/enjoy';
+import './scripts/enjoy';
 import '/styles/enjoy.scss'
 import '/styles/about.scss'
 import '/styles/mobile-app.scss'
 import '/styles/footer.scss'
 
-generateEnjoyList()
