69d788d5c77d1e2a584e0d85a0d00663d85a6a55
Pavel <verbipo@gmail.com>
2024-01-10 15:44:52 +0100

    feat: Implementation of the carousel on the home page: +24

--- scripts/enjoy.js
+++ scripts/enjoy.js
@@ -1,5 +1,18 @@
 import { coffeListMain } from '../data/coffe-slider';
 
+let intervalId;
+let progressBarWidth = 0;
+let startTime;
+let pausedTime = 0;
+let isPaused = false;
+let currentSlideIndex = 0;
+let sliderTranslation = 0;
+
+const dotsFill = document.querySelectorAll(".dot-fill");
+const dots = document.querySelectorAll(".dot")
+const slideList = document.getElementById('slider-list')
+
+
 function generateEnjoyList() {
   const sliderList = document.getElementById('slider-list')
 
@@ -38,15 +51,9 @@ function generateEnjoyList() {
 
 generateEnjoyList()
 
-let currentSlideIndex = 0;
-const dots = document.querySelectorAll(".dot")
-dots[currentSlideIndex].classList.add('active')
-
-const slideList = document.getElementById('slider-list')
-
-let sliderTranslation = 0;
-
 function currentSlide(index) {
+  progressBarWidth = 0
+  dotsFill[currentSlideIndex].style.width = 0
   currentSlideIndex = index
   sliderTranslation = -(index * 100)
   slideList.style.transform = `translateX(${sliderTranslation}%)`;
@@ -57,25 +64,22 @@ function currentSlide(index) {
 }
 
 function nextSlide() {
+  progressBarWidth = 0
+  dotsFill[currentSlideIndex].style.width = 0
   currentSlideIndex = (currentSlideIndex + 1) % dots.length;
   sliderTranslation = - (currentSlideIndex * 100);
   slideList.style.transform = `translateX(${sliderTranslation}%)`;
-  updateDots();
+  startInterval();
 }
 
 function prevSlide() {
+  progressBarWidth = 0
+  dotsFill[currentSlideIndex].style.width = 0
   currentSlideIndex = (currentSlideIndex - 1 + dots.length) % dots.length;
   sliderTranslation = - (currentSlideIndex * 100);
   console.log(currentSlideIndex)
   slideList.style.transform = `translateX(${sliderTranslation}%)`;
-  updateDots();
-}
-
-function updateDots() {
-  dots.forEach(element => {
-    element.classList.remove('active');
-  });
-  dots[currentSlideIndex].classList.add('active');
+  startInterval();
 }
 
 document.getElementById('enjoy-next-button').addEventListener('click', nextSlide);
@@ -83,3 +87,69 @@ document.getElementById('enjoy-prev-button').addEventListener('click', prevSlide
 dots.forEach((element, index) => {
   element.addEventListener('click', () => currentSlide(index));
 });
+
+function startInterval() {
+  startTime = Date.now() - pausedTime;
+
+  function updateProgressBar() {
+    if (!isPaused) {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - startTime;
+      progressBarWidth = (elapsedTime / 5000) * 100; // Use 5000 milliseconds (5 seconds) for the slide interval
+      dotsFill[currentSlideIndex].style.width = progressBarWidth + "%";
+      console.log('i work');
+      if (progressBarWidth < 100) {
+        requestAnimationFrame(updateProgressBar);
+      } else {
+        progressBarWidth = 0;
+        dotsFill[currentSlideIndex].style.width = 0;
+        nextSlide();
+        startTime = Date.now(); // Reset startTime after completing a slide
+      }
+    } else {
+      requestAnimationFrame(updateProgressBar);
+    }
+  }
+  intervalId = requestAnimationFrame(updateProgressBar);
+}
+
+startInterval();
+
+let touchstartX = 0;
+let touchendX = 0;
+
+function checkDirection() {
+  if (touchendX < touchstartX) nextSlide();
+  if (touchendX > touchstartX) prevSlide();
+}
+
+const sliderContainer = document.getElementById('slider-container');
+
+sliderContainer.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX;
+  pauseInterval();
+});
+
+sliderContainer.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX;
+  resumeInterval();
+  checkDirection();
+});
+
+sliderContainer.addEventListener('mouseover', () => {
+  pauseInterval();
+});
+
+sliderContainer.addEventListener('mouseout', () => {
+  resumeInterval();
+});
+
+function pauseInterval() {
+  isPaused = true;
+  pausedTime = Date.now() - startTime;
+}
+
+function resumeInterval() {
+  isPaused = false;
+  startInterval();
+}
